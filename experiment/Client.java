3c6a05e9862a37d24076fc66a0d8c7758906d122
Sophia Sun <sophiaphia@live.com>
2017-04-04 19:16:42 -0400

    4/4 first attempt

---
+++ experiment/Client.java
@@ -0,0 +1,30 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+
+    private Client() {}
+
+    public static void main(String[] args) {
+
+    // assuming takes in a list of IPs
+
+
+        String vmIPs = (args.length < 1) ? null : args;
+
+        try {
+
+            String vmAIP = vmIPs[0];
+            Registry vmARegistry = LocateRegistry.getRegistry(vmAIP);
+            VMInterface vmAStub = (VMInterface) registry.lookup("CloudSource");
+
+            String response = vmAStub.getCapacityWith(vmIPs[1]);
+
+            System.out.println("response: " + response);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
